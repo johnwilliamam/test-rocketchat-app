5473875c32b68d35e3ec49f909ddc10081da889f
John William <j,williamd.a.m@outlook.com>
2023-06-20 18:01:22 -0300

    add changes hook Rocketchat

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,13 +1,27 @@
-'use client'
-import { socketInitialize } from '@/services/socket';
-import { driver } from '@rocket.chat/sdk';
-import { useState } from 'react';
-import styles from './page.module.css';
+import { driver } from "@rocket.chat/sdk";
 
+export async function socketInitialize() {
+    const HOST = 'http://localhost:3000';
+    const USER = 'john.william';
+    const PASS = 'JWAM@2505';
+    const BOTNAME = 'easybot';  // name  bot response to
+    const SSL = false;  // server uses https ?
+    const ROOMS = ['GENERAL', 'myroom1'];
+    var myuserid;
 
-export default async function Home() {
-  const [name, setName] = useState()
-  async function listenMessage() {
+    try {
+        const conn = await driver.connect({ host: HOST, useSsl: SSL })
+        console.log('connect')
+        myuserid = await driver.login({ username: USER, password: PASS });
+        console.log('joined rooms');
+        // set up subscriptions - rooms we are interested in listening to
+        const subscribed = await driver.subscribeToMessages();
+        console.log('subscribed');
+    } catch (err) { }
+
+}
+
+export async function listenMessage() {
     // connect the processMessages callback
     const msgloop = await driver.reactToMessages(async (err, message, messageOptions) => {
         if (!err) {
@@ -21,42 +35,16 @@ export default async function Home() {
             //         message.msg.substr(BOTNAME.length + 1);
             //   const sentmsg = await driver.sendToRoom(response, roomname);
             console.log(message)
-            setName(message.msg)
         }
     });
     console.log('connected and waiting for messages');
-  
+
     // when a message is created in one of the ROOMS, we 
     // receive it in the processMesssages callback
-  
+
     // greets from the first room in ROOMS 
     // const sent = await driver.sendToRoom( BOTNAME + ' is listening ...',ROOMS[0]);
     // console.log('Greeting message sent');
-  
+
     // callback for incoming messages filter and processing
-  }
-  
-  const init = await socketInitialize();
-  console.log(init)
-  await listenMessage()
-  // const msg = await listenMessage()
-  // console.log(msg)
-  
-  // useEffect(() => {
-  //   async function messages(){
-  //     console.log('MSG', msg)
-  //   }
-  //   messages()
-  // })
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>Mensagem: {name} </div>
-      </div>
-    </main>
-  )
 }
