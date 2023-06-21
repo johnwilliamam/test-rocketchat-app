671704ccd3a0275a280482f2b461fd4de6b9f93b
John William <j,williamd.a.m@outlook.com>
2023-06-21 11:56:43 -0300

    Real time API from Rocketchat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,32 @@
 'use client'
-import { socketInitialize } from '@/services/socket';
-import { driver } from '@rocket.chat/sdk';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { RealTimeAPI } from 'rocket.chat.realtime.api.rxjs';
 import styles from './page.module.css';
+ 
 
-
-export default async function Home() {
+export default function Home() {
   const [name, setName] = useState()
-  async function listenMessage() {
-    // connect the processMessages callback
-    const msgloop = await driver.reactToMessages(async (err, message, messageOptions) => {
-        if (!err) {
-            // filter our own message
-            // if (message.u._id === myuserid) return;
-            // can filter further based on message.rid
-            // const roomname = await driver.getRoomName(message.rid);
-            // if (message.msg.toLowerCase().startsWith(BOTNAME)) {
-            //   const response = message.u.username + 
-            //         ', how can ' + BOTNAME + ' help you with ' +
-            //         message.msg.substr(BOTNAME.length + 1);
-            //   const sentmsg = await driver.sendToRoom(response, roomname);
-            console.log(message)
-            setName(message.msg)
-        }
-    });
-    console.log('connected and waiting for messages');
-  
-    // when a message is created in one of the ROOMS, we 
-    // receive it in the processMesssages callback
-  
-    // greets from the first room in ROOMS 
-    // const sent = await driver.sendToRoom( BOTNAME + ' is listening ...',ROOMS[0]);
-    // console.log('Greeting message sent');
-  
-    // callback for incoming messages filter and processing
-  }
-  
-  const init = await socketInitialize();
-  console.log(init)
-  await listenMessage()
-  // const msg = await listenMessage()
-  // console.log(msg)
-  
-  // useEffect(() => {
-  //   async function messages(){
-  //     console.log('MSG', msg)
-  //   }
-  //   messages()
-  // })
+
+  // socketInitialize()
+  const ws = new RealTimeAPI('ws://localhost:3000/websocket')
+  ws.connectToServer()
+  // ws.keepAlive().subscribe()
+  const auth = ws.login('john.william', 'JWAM@2505')
+
+  useEffect(() => {
+    async function websocket(){
+      ws.subscribe(
+        (data)=>console.log('DATA', data),
+        (error)=>console.log('ERR', error),
+        ()=>console.log('completed')
+      )
+      ws.onMessage((message) => {
+        console.log('MESSAGE', message)
+      })
+    
+    }
+    websocket()
+  })
   return (
     <main className={styles.main}>
       <div className={styles.description}>
--- src/services/socket.ts
+++ src/services/socket.ts
@@ -18,33 +18,35 @@ export async function socketInitialize() {
         const subscribed = await driver.subscribeToMessages();
         console.log('subscribed');
     } catch (err) { }
-
+    
 }
 
 export async function listenMessage() {
-    // connect the processMessages callback
-    const msgloop = await driver.reactToMessages(async (err, message, messageOptions) => {
-        if (!err) {
-            // filter our own message
-            // if (message.u._id === myuserid) return;
-            // can filter further based on message.rid
-            // const roomname = await driver.getRoomName(message.rid);
-            // if (message.msg.toLowerCase().startsWith(BOTNAME)) {
-            //   const response = message.u.username + 
-            //         ', how can ' + BOTNAME + ' help you with ' +
-            //         message.msg.substr(BOTNAME.length + 1);
-            //   const sentmsg = await driver.sendToRoom(response, roomname);
-            console.log(message)
-        }
-    });
-    console.log('connected and waiting for messages');
-
-    // when a message is created in one of the ROOMS, we 
-    // receive it in the processMesssages callback
-
-    // greets from the first room in ROOMS 
-    // const sent = await driver.sendToRoom( BOTNAME + ' is listening ...',ROOMS[0]);
-    // console.log('Greeting message sent');
-
-    // callback for incoming messages filter and processing
-}
+    try{ 
+        // connect the processMessages callback
+        const msgloop = await driver.reactToMessages(async (err, message, messageOptions) => {
+            if (!err) {
+                // filter our own message
+                // if (message.u._id === myuserid) return;
+                // can filter further based on message.rid
+                // const roomname = await driver.getRoomName(message.rid);
+                // if (message.msg.toLowerCase().startsWith(BOTNAME)) {
+                    //   const response = message.u.username + 
+                    //         ', how can ' + BOTNAME + ' help you with ' +
+                    //         message.msg.substr(BOTNAME.length + 1);
+                    //   const sentmsg = await driver.sendToRoom(response, roomname);
+                    console.log(message)
+                }
+            });
+            console.log('connected and waiting for messages');
+            
+            // when a message is created in one of the ROOMS, we 
+            // receive it in the processMesssages callback
+            
+            // greets from the first room in ROOMS 
+            // const sent = await driver.sendToRoom( BOTNAME + ' is listening ...',ROOMS[0]);
+            // console.log('Greeting message sent');
+            
+            // callback for incoming messages filter and processing
+        }catch(err) { }
+    }
